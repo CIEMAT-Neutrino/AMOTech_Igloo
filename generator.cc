67d6d5c6ce61b68dfd7dfecea8dd6a8fcb1d85c2
BeymarJ <beymarjavier.quenallata@ciemat.es>
2023-06-16 09:18:51 +0200

    Attempt to read SQLite files

--- generator.cc
+++ generator.cc
@@ -1,6 +1,6 @@
 #include "generator.hh"
 
-using namespace CLHEP;
+//using namespace CLHEP;
 
 MyPrimaryGenerator::MyPrimaryGenerator(){
 	fParticleGun = new G4ParticleGun(1);
@@ -8,10 +8,8 @@ MyPrimaryGenerator::MyPrimaryGenerator(){
 	G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
 	G4String particleName = "neutron";
 	G4ParticleDefinition *particle = particleTable->FindParticle("neutron");
-
 	
-	
-	fParticleGun->SetParticleEnergy(1.*MeV);
+	fParticleGun->SetParticleEnergy(100.*MeV);
 	fParticleGun->SetParticleDefinition(particle);
 }
 
@@ -20,22 +18,36 @@ MyPrimaryGenerator::~MyPrimaryGenerator(){
 }
 
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent){
-	//phi = G4UniformRand()*2.0*pi;
-	//cosTheta = G4UniformRand();
-	//sinTheta = sqrt(1.0 - cosTheta*cosTheta);
-	//G4ThreeVector pos(rad*TMath::Cos(phi)*sinTheta, rad*cosTheta ,rad*TMath::Sin(phi)*sinTheta); //Horizonal axes are x and z, vertical is y
-	
-	G4double Srand = (G4UniformRand()*2-1)*(2.4);
-	G4double Lrand = (G4UniformRand()*2-1)*(4.0);
-	G4double H = 7.;
-	G4ThreeVector pos(Srand*m, H*m, Lrand*m); //Horizonal axes are x and z, vertical is y
-	
-	G4ThreeVector down = G4ThreeVector(0., -1., 0.);
-	G4ThreeVector mom = -1.0*pos;
 
-	fParticleGun->SetParticlePosition(pos);
-	fParticleGun->SetParticleMomentumDirection(down);
+	rc = sqlite3_open("Data/p_showers_210200.db", &db);
 
-	fParticleGun->GeneratePrimaryVertex(anEvent);
+	if(rc){
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		defaultGenerator = true;
+	}else{
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	sqlite3_close(db);
 
+	if(defaultGenerator){
+		/* Another default generation
+		G4double Srand = (G4UniformRand()*2-1)*(2.4);
+		G4double Lrand = (G4UniformRand()*2-1)*(4.0);
+		G4double H = 7.;
+		G4ThreeVector pos(Srand*m, H*m, Lrand*m); //Horizonal axes are x and z, vertical is y
+		*/
+
+		phi = G4UniformRand()*2.0*pi;
+		cosTheta = G4UniformRand();
+		sinTheta = sqrt(1.0 - cosTheta*cosTheta);
+		G4ThreeVector pos(rad*TMath::Cos(phi)*sinTheta, rad*cosTheta ,rad*TMath::Sin(phi)*sinTheta); //Horizonal axes are x and z, vertical is y
+
+		G4ThreeVector down = G4ThreeVector(0., -1., 0.);
+		G4ThreeVector mom = -1.0*pos;
+
+		fParticleGun->SetParticlePosition(pos);
+		fParticleGun->SetParticleMomentumDirection(mom);
+
+		fParticleGun->GeneratePrimaryVertex(anEvent);
+	}
 }
--- generator.hh
+++ generator.hh
@@ -9,6 +9,11 @@
 #include "G4SystemOfUnits.hh"
 #include "G4ParticleTable.hh"
 
+#include <stdio.h>
+#include <sqlite3.h>
+#include <stdlib.h>
+//#include "ifdh.h"
+
 #include "TMath.h"
 
 class MyPrimaryGenerator : public G4VUserPrimaryGeneratorAction{
@@ -19,12 +24,19 @@ public:
 
 	virtual void GeneratePrimaries(G4Event*);
 
+
 private:
 	G4ParticleGun *fParticleGun;
 
 	G4double rad = 8*m;
 	G4double phi, cosTheta, sinTheta; 
-	
+
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+
+	bool defaultGenerator = false;
+
 };
 
 #endif
